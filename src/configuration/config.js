a130221b91c171f46e798d60fff3055235834ae4
Naeem Akhter <dreamabroad83@gmail.com>
2025-03-27 12:33:34 +0530

    congif changes

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -2,10 +2,10 @@ const mode = process.env.DEVELOPMENT === 'development'
 
 
 module.exports = {
-    PORT : mode ? Number(process.env.PORT) || 8081 : null,
-    MONGODB_URI : mode ? process.env.DATABASE_CONNECTION_STRING : null,
-    ACCESS_TOKEN_SECRET_KEY : mode ? process.env.ACCESS_TOKEN_SECRET_KEY : null,
-    ACCESS_TOKEN_EXPIRY : mode ? process.env.ACCESS_TOKEN_EXPIRY : null,
-    REFERECE_TOKEN_SECRET_KEY : mode ? process.env.REFERECE_TOKEN_SECRET_KEY : null,
-    REFRESH_TOKEN_EXPIRY : mode ? process.env.REFRESH_TOKEN_EXPIRY : null,
+    PORT : mode ? Number(process.env.PORT) || 8081 : Number(process.env.PORT),
+    MONGODB_URI : mode ? process.env.DATABASE_CONNECTION_STRING : process.env.PRODUCTION_DATABASE_CONNECTION_STRING,
+    ACCESS_TOKEN_SECRET_KEY : mode ? process.env.ACCESS_TOKEN_SECRET_KEY : process.env.PRODUCTION_ACCESS_TOKEN_SECRET_KEY,
+    ACCESS_TOKEN_EXPIRY : mode ? process.env.ACCESS_TOKEN_EXPIRY : process.env.PRODUCTION_ACCESS_TOKEN_EXPIRY,
+    REFERECE_TOKEN_SECRET_KEY : mode ? process.env.REFERECE_TOKEN_SECRET_KEY : process.env.PRODUCTION_REFERECE_TOKEN_SECRET_KEY,
+    REFRESH_TOKEN_EXPIRY : mode ? process.env.REFRESH_TOKEN_EXPIRY : process.env.PRODUCTION_REFRESH_TOKEN_EXPIRY,
 }
